7e7dc7807c8ef74154aa12265e01b0faa0aa1037
blithe-hacker-0535 <sadiebenn1@gmail.com>
2018-12-18 23:01:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
-# def square_array(array)
-#   squares = []
-#   array.each { |num| squares << num * num }
-#   return squares
-# end
-
 def square_array(array)
-  squares = array.collect {|num| num * num}
+  squares = []
+  array.each { |num| squares << num * num }
   return squares
 end
+
+# def square_array(array)
+#   array.collect {|num| num * num}
+# end
